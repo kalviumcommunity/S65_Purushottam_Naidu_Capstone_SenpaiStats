c46af0a50c4d63b348b76bedecf53016b03a4e43
DippamTheFirst <dippamthefirst@gmail.com>
2025-12-06 10:00:18 +0530

    added google auth

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,54 +1,97 @@
-const User = require('../models/userModel')
+const User = require("../models/userModel");
+
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("./models/User");
+const passport = require("passport");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      scope: ["profile", "email"],
+    },
+    async function (accessToken, refreshToken, profile, done) {
+      try {
+        const existingUser = await User.findOne({
+          Email: profile.emails[0].value,
+        });
+
+        if (existingUser) {
+          return done(null, existingUser);
+        }
+
+        const newUser = new User({
+          Email: profile.emails[0].value,
+          GoogleId: profile.id,
+        });
+
+        await newUser.save();
+        newUser.isNewUser = true;
+
+        return done(null, newUser);
+      } catch (err) {
+        return done(err, null);
+      }
+    }
+  )
+);
 
 const getUsers = async (req, res) => {
-    try {
-        const users = await User.findOne()
-        if (!users) {
-            return res.status(400).json({message: "No users found!"})
-        }
-        return res.status(200).json({userDetails: users})
-    } catch (err) {
-        return res.status(500).json({error: err.message})
+  try {
+    const users = await User.findOne();
+    if (!users) {
+      return res.status(400).json({ message: "No users found!" });
     }
-}
+    return res.status(200).json({ userDetails: users });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
 
 const addUser = async (req, res) => {
-    try {
-        const {username, email, password} = req.body
+  try {
+    const { username, email, password } = req.body;
 
-        if (!username || !email || !password) {
-            return res.status(400).json({message: "All fields are required!"})
-        }
-        const newUser = new User({
-            username,
-            email,
-            password
-        })
-
-        await newUser.save()
-        return res.status(201).json({message: "User Created Successfully...", userDetails: newUser})
-        
-    } catch (err) {
-        return res.status(500).json({error: err.message})
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required!" });
     }
-}
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
+
+    await newUser.save();
+    return res
+      .status(201)
+      .json({ message: "User Created Successfully...", userDetails: newUser });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
 
 const updateUser = async (req, res) => {
-    try {
-        const {_id} = req.params
+  try {
+    const { _id } = req.params;
 
-        const existingUser = await User.findOne({_id})
+    const existingUser = await User.findOne({ _id });
 
-        if (!existingUser) {
-            return res.status(400).json({message: "Not a valid user!"})
-        }
-        
-        const updatedUser = await User.findByIdAndUpdate(_id, req.body, {new: true})
-        return res.status(200).json({message: "User Updation Successfull...", userDetails: updatedUser})
-
-    } catch (err) {
-        return res.status(500).json({error: err.message})
+    if (!existingUser) {
+      return res.status(400).json({ message: "Not a valid user!" });
     }
-}
 
-module.exports = {getUsers, addUser, updateUser}
+    const updatedUser = await User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+    });
+    return res.status(200).json({
+      message: "User Updation Successfull...",
+      userDetails: updatedUser,
+    });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = { getUsers, addUser, updateUser };
